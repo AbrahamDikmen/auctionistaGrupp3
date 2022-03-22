93d3a75b94eea79c196c768458fc677a3c08780d
lunndin <70664281+lunndin@users.noreply.github.com>
2022-03-22 16:47:36 +0100

    Co-authored-by: L00phole <L00phole@users.noreply.github.com> Co-authored-by: Adam Lundstedt <adamLundstedt@users.noreply.github.com> Co-authored-by: Jennie Werther Johansson <denjennie@users.noreply.github.com> Co-authored-by: Ali Lilo <AliLilo95@users.noreply.github.com> Co-authored-by: Antonio Gallardo Girela <herrgallardo@users.noreply.github.com>

--- server.js
+++ server.js
@@ -5,25 +5,25 @@ server.use(express.json());
 
 // middleware (exempel på hur detta med "use" fungerar, som i server.use)
 server.use((request, response, next) => {
-  request.hello = "world";
-  response.header("X-goodbye", "cruel world");
-  next();
+    request.hello = "world";
+    response.header("X-goodbye", "cruel world");
+    next();
 });
 
 // lägg till session-hantering
 session = require("express-session");
 server.use(
-  session({
-    secret: ".l,rtkdyfhgs.xdsdalkrdfgkcdhmsrfkx", // för att salta våra session ids
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: false }, // vi SKA använda secure cookies i produktion, MEN INTE i dev
-  })
+    session({
+        secret: ".l,rtkdyfhgs.xdsdalkrdfgkcdhmsrfkx", // för att salta våra session ids
+        resave: false,
+        saveUninitialized: true,
+        cookie: { secure: false }, // vi SKA använda secure cookies i produktion, MEN INTE i dev
+    })
 );
 
 // starta servern
 server.listen(3000, () => {
-  console.log("server started at http://localhost:3000/data");
+    console.log("server started at http://localhost:3000/data");
 });
 
 // data
@@ -33,7 +33,7 @@ const db = new sqlite3.Database("./database/auctionista.db");
 db.all = util.promisify(db.all);
 db.run = util.promisify(db.run);
 
-const menuItems = require("./menu-items.json");
 const req = require("express/lib/request");
 // Antonio was hereS
 // Jennie is best
+
