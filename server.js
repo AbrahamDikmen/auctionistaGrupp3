4b5346b5cd9910387a793d963a05653dee0eef2d
Abraham Dikmen <abraham.dikmen@outlook.com>
2022-03-24 09:48:38 +0100

    update

--- server.js
+++ server.js
@@ -8,30 +8,13 @@ server.use((request, response, next) => {
   request.hello = "world";
   response.header("X-goodbye", "cruel world");
   next();
+  request.hello = "world";
+  response.header("X-goodbye", "cruel world");
+  next();
 });
 
 // lägg till session-hantering
 session = require("express-session");
-server.use(
-  session({
-    secret: ".l,rtkdyfhgs.xdsdalkrdfgkcdhmsrfkx", // för att salta våra session ids
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: false }, // vi SKA använda secure cookies i produktion, MEN INTE i dev
-  })
-);
-
-// starta servern
-server.listen(3000, () => {
-  console.log("server started at http://localhost:3000/data");
-});
-
-// data
-const util = require("util");
-const sqlite3 = require("sqlite3");
-const db = new sqlite3.Database("./database/auctionista.db");
-db.all = util.promisify(db.all);
-db.run = util.promisify(db.run);
 
 const req = require("express/lib/request");
 
