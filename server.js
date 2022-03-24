179f4e97781ce40292eea4ca6501a8d4a2ae1001
herrgallardo <gallardogirela@gmail.com>
2022-03-24 10:14:22 +0100

    added code after failed merge attempt

--- server.js
+++ server.js
@@ -21,7 +21,6 @@ server.use(
   })
 );
 
-
 // starta servern
 server.listen(3000, () => {
   console.log("server started at http://localhost:3000/data");
@@ -37,42 +36,62 @@ db.run = util.promisify(db.run);
 const req = require("express/lib/request");
 
 //Som besökare vill jag kunna se sammanfattade auktionsobjekt som en lista. / J&M
-server.get('/data/objekt/summary-list', async (request, response) => {
-    let query = "SELECT titel, start_pris, bild FROM objekt"
-    let result = await db.all(query)
-    response.json(result)
-})
+server.get("/data/objekt/summary-list", async (request, response) => {
+  let query = "SELECT titel, start_pris, bild FROM objekt";
+  let result = await db.all(query);
+  response.json(result);
+});
 //Som besökare vill jag kunna se detaljer för varje auktionsobjekt. / J&M
-server.get('/data/objekt/details', async (request, response) => {
-    let query = 'SELECT anvandare.anvandarnamn, objekt.saljare, objekt.beskrivning, objekt.titel, objekt.kategori, objekt.start_tid, objekt.slut_tid, objekt.bild, objekt.start_pris, objekt.status FROM objekt,anvandare WHERE anvandare.id = objekt.saljare'
-    let result = await db.all(query)
-    response.json(result)
-})
+server.get("/data/objekt/details", async (request, response) => {
+  let query =
+    "SELECT anvandare.anvandarnamn, objekt.saljare, objekt.beskrivning, objekt.titel, objekt.kategori, objekt.start_tid, objekt.slut_tid, objekt.bild, objekt.start_pris, objekt.status FROM objekt,anvandare WHERE anvandare.id = objekt.saljare";
+  let result = await db.all(query);
+  response.json(result);
+});
 //Som besökare vill jag kunna se nuvarande bud på auktionsobjekt i listvyer / J&M
-server.get('/data/objekt/bid-list', async (request, response) => {
-    let query = 'SELECT titel, bud_pris FROM objekt, bud WHERE objekt.id = bud.id;'
-    let result = await db.all(query)
-    response.json(result)
-})
+server.get("/data/objekt/bid-list", async (request, response) => {
+  let query =
+    "SELECT titel, bud_pris FROM objekt, bud WHERE objekt.id = bud.id;";
+  let result = await db.all(query);
+  response.json(result);
+});
+
+// Alla bud
+server.get("/data/bud", async (request, response) => {
+  let query =
+    "SELECT bud.objekt_id, objekt.titel, anvandare.anvandarnamn AS budgivare, bud.bud_pris ||' SEK' AS bud_pris, bud.bud_tid, status.status FROM bud, anvandare, objekt, status WHERE bud.bud_givare = anvandare.id AND bud.objekt_id = objekt.id AND objekt.status = status.id";
+  let result = await db.all(query);
+  response.json(result);
+});
+
+// Som besökare vill jag kunna se nuvarande bud på auktionsobjekt i detaljsidor.
+// http://localhost:3000/data/bud/1
+server.get("/data/bud/:objekt_id", async (request, response) => {
+  // request.params.objekt_id === 1
+
+  let query =
+    "SELECT bud.objekt_id, objekt.titel, anvandare.anvandarnamn AS budgivare, bud.bud_pris ||' SEK' AS bud_pris, bud.bud_tid, status.status FROM bud, anvandare, objekt, status WHERE bud.bud_givare = anvandare.id AND bud.objekt_id = objekt.id AND objekt.status = status.id AND bud.objekt_id = ?";
+  let result = await db.all(query, [request.params.objekt_id]);
+  response.json(result);
+});
 
 server.post("/data/login", async (request, response) => {
-
   let query = await db.all(
     "SELECT id, anvandarnamn, losenord FROM Anvandare WHERE anvandarnamn = ? AND losenord = ?",
     [request.body.anvandarnamn, request.body.losenord]
-  )
+  );
 
   /*
     OM query arrayn är större än 0
     Lägg till Användaren i session 
     skicka tillbaka användaren
   */
-  
+
   if (query.length > 0) {
-    request.session.query = query[0]
-    response.json(query[0])
+    request.session.query = query[0];
+    response.json(query[0]);
     return;
   } else {
-    response.json({ "status": "Wrong Username/Password"})
+    response.json({ status: "Wrong Username/Password" });
   }
-})
+});
