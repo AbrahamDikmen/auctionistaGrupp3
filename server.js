741649c5a0d8579c65dbc68fb0ca4a02556d8858
AliLiloo <alililo95@outlook.com>
2022-03-24 10:02:03 +0100

    Alitest

--- server.js
+++ server.js
@@ -21,6 +21,30 @@ server.use(
   })
 );
 
+
+server.post("/data/login", async (request, response) => {
+
+  let query = await db.all(
+    "SELECT id, anvandarnamn, losenord FROM Anvandare WHERE anvandarnamn = ? AND losenord = ?",
+    [request.body.anvandarnamn, request.body.losenord]
+  )
+
+  /*
+    OM query arrayn är större än 0
+    Lägg till Användaren i session 
+    skicka tillbaka användaren
+  */
+  
+  if (query.length > 0) {
+    request.session.query = query[0]
+    response.json(query[0])
+    return;
+  } else {
+    response.json({ "status": "Wrong Username/Password"})
+  }
+})
+
+
 // starta servern
 server.listen(3000, () => {
   console.log("server started at http://localhost:3000/data");
