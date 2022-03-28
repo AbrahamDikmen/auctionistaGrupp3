56024a345aa82ee82f1e3ebece472f21379516f8
Adam Lundstedt <89978751+adamLundstedt@users.noreply.github.com>
2022-03-28 15:12:26 +0200

    updated feature messages

--- feature_message.js
+++ feature_message.js
@@ -12,8 +12,16 @@ server.post('/data/conversation/new_conversation', async (request, response)=>{
 
 // Reply messages
 // Update the conversation with new messages in a already existing conversation.
-server.put('/data/conversation/:id', async (request, response)=>{
+server.put('/data/conversation/reply_:id', async (request, response)=>{
     let query = "UPDATE messages SET message = message || ' ' || CHAR(13) || CHAR(13) || CURRENT_DATE || ' ' || CURRENT_TIME || ':  ' || CHAR(13) || ? WHERE id = ?"
     await db.run(query, [request.body.message, request.params.id])
     response.json({result: "Your reply has been sent."})
+})
+
+// Read conversation
+// A get for reading the conversation.
+server.get('/data/conversation/:id', async (request, response)=>{
+    let query = "SELECT message FROM messages WHERE id = ?"
+    let result = await db.all(query, [request.params.id])
+    response.json(result)
 })
