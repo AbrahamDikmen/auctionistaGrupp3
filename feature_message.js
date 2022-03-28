3bd519822ff6bd8e0babcefdb7419b7c157764a8
Adam Lundstedt <89978751+adamLundstedt@users.noreply.github.com>
2022-03-28 15:23:45 +0200

    :)

--- feature_message.js
+++ feature_message.js
@@ -7,7 +7,11 @@
 // Creates the connection between two users.
 server.post('/data/conversation/new_conversation', async (request, response)=>{
     let query = "INSERT INTO messages (sender, reciver, message, object) VALUES(?,?, CURRENT_DATE || ' ' || CURRENT_TIME || ':  ' || CHAR(13) || ?, ? )"
-    await db.run(query, [request.body.sender, request.body.reciver, request.body.message, request.body.object])
+    await db.run(query, [
+        request.body.sender,
+        request.body.reciver,
+        request.body.message,
+        request.body.object])
     response.json({result: "Your message has been sent."})
 })
 
@@ -15,7 +19,9 @@ server.post('/data/conversation/new_conversation', async (request, response)=>{
 // Update the conversation with new messages in a already existing conversation.
 server.put('/data/conversation/reply_:id', async (request, response)=>{
     let query = "UPDATE messages SET message = message || ' ' || CHAR(13) || CHAR(13) || CURRENT_DATE || ' ' || CURRENT_TIME || ':  ' || CHAR(13) || ? WHERE id = ?"
-    await db.run(query, [request.body.message, request.params.id])
+    await db.run(query, [
+        request.body.message,
+        request.params.id])
     response.json({result: "Your reply has been sent."})
 })
 
