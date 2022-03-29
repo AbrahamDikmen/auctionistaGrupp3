3cf18ae630e5d23e81f0c19e1c53def257e7c945
denjennie <denjennie@live.se>
2022-03-29 08:58:41 +0200

    merge

--- server.js
+++ server.js
@@ -94,20 +94,20 @@ server.get("/data/anvandare", async (request, response) => {
   response.json(result);
 });
 
-//Som besökare vill jag kunna se sammanfattade auktionsobjekt som en lista. / J&M
+//Som besökare vill jag kunna se sammanfattade auktionsobjekt som en lista. 
 server.get("/data/objekt/summary-list", async (request, response) => {
   let query = "SELECT titel, start_pris, bild FROM objekt";
   let result = await db.all(query);
   response.json(result);
 });
-//Som besökare vill jag kunna se detaljer för varje auktionsobjekt. / J&M
+//Som besökare vill jag kunna se detaljer för varje auktionsobjekt. 
 server.get("/data/objekt/details", async (request, response) => {
   let query =
     "SELECT anvandare.anvandarnamn, objekt.saljare, objekt.beskrivning, objekt.titel, objekt.kategori, objekt.start_tid, objekt.slut_tid, objekt.bild, objekt.start_pris, objekt.status FROM objekt,anvandare WHERE anvandare.id = objekt.saljare";
   let result = await db.all(query);
   response.json(result);
 });
-//Som besökare vill jag kunna se nuvarande bud på auktionsobjekt i listvyer / J&M
+//Som besökare vill jag kunna se nuvarande bud på auktionsobjekt i listvyer 
 server.get("/data/objekt/bid-list", async (request, response) => {
   let query =
     "SELECT titel, bud_pris FROM objekt, bud WHERE objekt.id = bud.id;";
