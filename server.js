0f46168a928971aa09d1347abaea2b87f70bf9dc
Nardos22 <94529625+Nardos22@users.noreply.github.com>
2022-03-29 13:57:55 +0200

    review on owner

--- server.js
+++ server.js
@@ -417,3 +417,13 @@ server.get('/data/conversation/:id', async (request, response)=>{
     let result = await db.all(query, [request.params.id])
     response.json(result)
 })
+
+// 24.Som användare vill jag kunna se säljares betyg när jag tittar på ett auktionsobjekt
+server.get('/data/saljarens-betyg/:objektId', async (request, response)=>{
+  let query = `SELECT titel, beskrivning, betyg.betyg 
+                 FROM objekt
+                 JOIN betyg ON objekt.saljare = betyg.anvandare_id
+                 WHERE objekt.id = ?`
+  let result = await db.all(query, [request.params.objektId])
+  response.json(result)
+})
