495e2259c0d55549aa53aef3f9f468d8576adf59
herrgallardo <gallardogirela@gmail.com>
2022-03-29 09:08:11 +0200

    sorted the code

--- server.js
+++ server.js
@@ -35,7 +35,22 @@ db.run = util.promisify(db.run);
 
 const req = require("express/lib/request");
 
-//3.
+// 1. Som besökare vill jag kunna se sammanfattade auktionsobjekt som en lista. / J&M
+server.get("/data/objekt/summary-list", async (request, response) => {
+  let query = "SELECT titel, start_pris, bild FROM objekt";
+  let result = await db.all(query);
+  response.json(result);
+});
+
+// 2. Som besökare vill jag kunna se detaljer för varje auktionsobjekt. / J&M
+server.get("/data/objekt/details", async (request, response) => {
+  let query =
+    "SELECT anvandare.anvandarnamn, objekt.saljare, objekt.beskrivning, objekt.titel, objekt.kategori, objekt.start_tid, objekt.slut_tid, objekt.bild, objekt.start_pris, objekt.status FROM objekt,anvandare WHERE anvandare.id = objekt.saljare";
+  let result = await db.all(query);
+  response.json(result);
+});
+
+//3. Som besökare vill jag kunna söka på auktioner baserat på vad jag skriver i ett sökfält.
 server.get("/data/search/:string", async (request, response) => {
   let query = `SELECT * FROM  objekt WHERE INSTR(LOWER(objekt.titel), LOWER(?))>0 OR INSTR(LOWER(objekt.beskrivning), LOWER(?))>0`;
   let result = await db.all(
@@ -44,6 +59,75 @@ server.get("/data/search/:string", async (request, response) => {
     request.params.string
   );
   response.json(result);
+});
+
+// 4. Som besökare vill jag kunna se nuvarande bud på auktionsobjekt i listvyer / J&M
+server.get("/data/objekt/bid-list", async (request, response) => {
+  let query =
+    "SELECT titel, bud_pris FROM objekt, bud WHERE objekt.id = bud.id;";
+  let result = await db.all(query);
+  response.json(result);
+});
+
+// Alla bud
+server.get("/data/bud", async (request, response) => {
+  let query =
+    "SELECT bud.objekt_id, objekt.titel, anvandare.anvandarnamn AS budgivare, bud.bud_pris ||' SEK' AS bud_pris, bud.bud_tid, status.status FROM bud, anvandare, objekt, status WHERE bud.bud_givare = anvandare.id AND bud.objekt_id = objekt.id AND objekt.status = status.id";
+  let result = await db.all(query);
+  response.json(result);
+});
+
+// 5. Som besökare vill jag kunna se nuvarande bud på auktionsobjekt i detaljsidor.
+// http://localhost:3000/data/bud/1
+server.get("/data/bud/:objekt_id", async (request, response) => {
+  // request.params.objekt_id === 1
+
+  let query =
+    "SELECT bud.objekt_id, objekt.titel, anvandare.anvandarnamn AS budgivare, bud.bud_pris ||' SEK' AS bud_pris, bud.bud_tid, status.status FROM bud, anvandare, objekt, status WHERE bud.bud_givare = anvandare.id AND bud.objekt_id = objekt.id AND objekt.status = status.id AND bud.objekt_id = ?";
+  let result = await db.all(query, [request.params.objekt_id]);
+  response.json(result);
+});
+
+// 6.Som besökare vill jag kunna registrera ett nytt konto och bli användare
+server.post("/data/anvandare", async (request, response) => {
+  let query = `INSERT INTO anvandare 
+    (namn, efternamn, anvandarnamn, losenord, telefonnummer, adress, postkod, ort, mail, bild) 
+      VALUES (?,?,?,?,?,?,?,?,?,?)`;
+  await db.run(query, [
+    request.body.namn,
+    request.body.efternamn,
+    request.body.anvandarnamn,
+    request.body.telefonnummer,
+    request.body.adress,
+    request.body.postkod,
+    request.body.ort,
+    request.body.mail,
+    request.body.bild,
+  ]);
+  response.json({ result: "A customer was added" });
+});
+
+// 7. Som användare vill jag kunna logga in. 
+
+server.post("/data/login", async (request, response) => {
+  let query = await db.all(
+    "SELECT id, anvandarnamn, losenord FROM Anvandare WHERE anvandarnamn = ? AND losenord = ?",
+    [request.body.anvandarnamn, request.body.losenord]
+  );
+
+  /*
+    OM query arrayn är större än 0
+    Lägg till Användaren i session 
+    skicka tillbaka användaren
+  */
+
+  if (query.length > 0) {
+    request.session.query = query[0];
+    response.json(query[0]);
+    return;
+  } else {
+    response.json({ status: "Wrong Username/Password" });
+  }
 });
 
 //8.10.
@@ -77,103 +161,6 @@ server.post("/data/bud", async (req, res) => {
   }
 });
 
-// 17.Som användare vill jag kunna se en lista med mina egna auktionsobjekt.
-server.get("/data/mina-auktioner/:id", async (request, response) => {
-  let query = `SELECT titel, kategorier.kategori, beskrivning
-               FROM objekt
-               JOIN kategorier ON objekt.kategori = kategorier.id
-               JOIN anvandare ON objekt.saljare = anvandare.id
-               WHERE anvandare.id = ? `;
-  let result = await db.all(query, [request.params.id]);
-  response.json(result);
-});
-
-server.get("/data/anvandare", async (request, response) => {
-  let query = "SELECT * FROM anvandare";
-  let result = await db.all(query);
-  response.json(result);
-});
-
-//Som besökare vill jag kunna se sammanfattade auktionsobjekt som en lista. / J&M
-server.get("/data/objekt/summary-list", async (request, response) => {
-  let query = "SELECT titel, start_pris, bild FROM objekt";
-  let result = await db.all(query);
-  response.json(result);
-});
-//Som besökare vill jag kunna se detaljer för varje auktionsobjekt. / J&M
-server.get("/data/objekt/details", async (request, response) => {
-  let query =
-    "SELECT anvandare.anvandarnamn, objekt.saljare, objekt.beskrivning, objekt.titel, objekt.kategori, objekt.start_tid, objekt.slut_tid, objekt.bild, objekt.start_pris, objekt.status FROM objekt,anvandare WHERE anvandare.id = objekt.saljare";
-  let result = await db.all(query);
-  response.json(result);
-});
-//Som besökare vill jag kunna se nuvarande bud på auktionsobjekt i listvyer / J&M
-server.get("/data/objekt/bid-list", async (request, response) => {
-  let query =
-    "SELECT titel, bud_pris FROM objekt, bud WHERE objekt.id = bud.id;";
-  let result = await db.all(query);
-  response.json(result);
-});
-
-// Alla bud
-server.get("/data/bud", async (request, response) => {
-  let query =
-    "SELECT bud.objekt_id, objekt.titel, anvandare.anvandarnamn AS budgivare, bud.bud_pris ||' SEK' AS bud_pris, bud.bud_tid, status.status FROM bud, anvandare, objekt, status WHERE bud.bud_givare = anvandare.id AND bud.objekt_id = objekt.id AND objekt.status = status.id";
-  let result = await db.all(query);
-  response.json(result);
-});
-
-// Som besökare vill jag kunna se nuvarande bud på auktionsobjekt i detaljsidor.
-// http://localhost:3000/data/bud/1
-server.get("/data/bud/:objekt_id", async (request, response) => {
-  // request.params.objekt_id === 1
-
-  let query =
-    "SELECT bud.objekt_id, objekt.titel, anvandare.anvandarnamn AS budgivare, bud.bud_pris ||' SEK' AS bud_pris, bud.bud_tid, status.status FROM bud, anvandare, objekt, status WHERE bud.bud_givare = anvandare.id AND bud.objekt_id = objekt.id AND objekt.status = status.id AND bud.objekt_id = ?";
-  let result = await db.all(query, [request.params.objekt_id]);
-  response.json(result);
-});
-
-// 6.Som besökare vill jag kunna registrera ett nytt konto och bli användare
-server.post("/data/anvandare", async (request, response) => {
-  let query = `INSERT INTO anvandare 
-    (namn, efternamn, anvandarnamn, losenord, telefonnummer, adress, postkod, ort, mail, bild) 
-      VALUES (?,?,?,?,?,?,?,?,?,?)`;
-  await db.run(query, [
-    request.body.namn,
-    request.body.efternamn,
-    request.body.anvandarnamn,
-    request.body.telefonnummer,
-    request.body.adress,
-    request.body.postkod,
-    request.body.ort,
-    request.body.mail,
-    request.body.bild,
-  ]);
-  response.json({ result: "A customer was added" });
-});
-
-server.post("/data/login", async (request, response) => {
-  let query = await db.all(
-    "SELECT id, anvandarnamn, losenord FROM Anvandare WHERE anvandarnamn = ? AND losenord = ?",
-    [request.body.anvandarnamn, request.body.losenord]
-  );
-
-  /*
-    OM query arrayn är större än 0
-    Lägg till Användaren i session 
-    skicka tillbaka användaren
-  */
-
-  if (query.length > 0) {
-    request.session.query = query[0];
-    response.json(query[0]);
-    return;
-  } else {
-    response.json({ status: "Wrong Username/Password" });
-  }
-});
-
 /*
 9. Som användare vill jag kunna skapa nya auktionsobjekt.
 11. Som användare vill jag att auktinsobjekt ska innehålla minst, titel,
@@ -201,14 +188,7 @@ server.post("/data/new_auction_object", async (request, response) => {
   response.json({ result: "One new auction object was created" });
 });
 
-//20.Som användare vill jag ha en publik profilsida där namn,
-//publika kontaktuppgift(er) & bild visas för andra att läsa.
-server.get("/data/anvandare/:id", async (request, response) => {
-  let query =
-    "SELECT bild, anvandarnamn, namn, mail FROM anvandare WHERE id = ?";
-  let result = await db.all(query, [request.params.id]);
-  response.json(result);
-});
+
 
 /* 
 14.Som besökare vill jag kunna se auktioner inom kategorier.
@@ -357,6 +337,23 @@ server.get("/data/status/:id", async (request, response) => {
   response.json(result);
 });
 
+// 17.Som användare vill jag kunna se en lista med mina egna auktionsobjekt.
+server.get("/data/mina-auktioner/:id", async (request, response) => {
+  let query = `SELECT titel, kategorier.kategori, beskrivning
+               FROM objekt
+               JOIN kategorier ON objekt.kategori = kategorier.id
+               JOIN anvandare ON objekt.saljare = anvandare.id
+               WHERE anvandare.id = ? `;
+  let result = await db.all(query, [request.params.id]);
+  response.json(result);
+});
+
+server.get("/data/anvandare", async (request, response) => {
+  let query = "SELECT * FROM anvandare";
+  let result = await db.all(query);
+  response.json(result);
+});
+
 // 18. Som användare vill jag kunna se en lista med auktionsobjekt jag har lagt bud på.
 
 server.get("/data/:anvandare/mina_bud", async (request, response) => {
@@ -365,6 +362,21 @@ server.get("/data/:anvandare/mina_bud", async (request, response) => {
   let result = await db.all(query, [request.params.anvandare]);
   response.json(result);
 });
+
+
+
+
+//20.Som användare vill jag ha en publik profilsida där namn,
+//publika kontaktuppgift(er) & bild visas för andra att läsa.
+server.get("/data/anvandare/:id", async (request, response) => {
+  let query =
+    "SELECT bild, anvandarnamn, namn, mail FROM anvandare WHERE id = ?";
+  let result = await db.all(query, [request.params.id]);
+  response.json(result);
+});
+
+
+
 
 // 22. Som köpare vill jag kunna ge ett betyg efter köp av ett auktionsobjekt.
 
